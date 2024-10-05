639327020c54aa58a2d804c168ca29635ff649db
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-10-05 14:12:53 -0400

    edited layout.tsx and started admin panel

---
+++ Client/app/(platform)/components/AdminHeader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaChevronDown, FaCog, FaQuestionCircle, FaUserAlt } from 'react-icons/fa';
+
+const AdminHeader = () => {
+    return (
+        <header className="flex items-center justify-between p-4 bg-white text-black w-full border-b border-black">            <div className="flex items-center">
+                <button className="mr-4 p-2 rounded hover:text-gray-950">
+                    <span className="text-2xl">☰</span>
+                </button>
+                <h1 className="text-xl font-bold">Admin Dashboard</h1>
+            </div>
+            <div className="flex items-center">
+                <button className="flex items-center rounded hover:text-gray-950 mr-2">
+                    <FaCog className="text-2xl mr-2" />
+                </button>
+                <button className="flex items-center rounded hover:text-gray-950 mr-2">
+                    <FaQuestionCircle className="text-2xl mr-2" />
+                </button>
+                <button className='flex items-center p-2 rounded hover:bg-gray-50 border border-black'>
+                    <FaUserAlt className="text-2xl mr-2" />
+                    <span>John Doe</span>
+                    <FaChevronDown className="text-2xl ml-2" /> 
+                </button>
+            </div>
+        </header>
+    );
+};
+
+export default AdminHeader;
--- Client/app/(platform)/layout.tsx
+++ Client/app/(platform)/layout.tsx
@@ -1,14 +1,24 @@
+"use client"; // Mark as a Client Component
+
+import { usePathname } from "next/navigation";
 import Sidebar from "./components/Sidebar";
+import AdminHeader from "./components/AdminHeader";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <main className="flex bg-[#111]  w-full h-full">
-        <Sidebar/>
-          {children}
-      </main>
-    );
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  const isAdminRoute = pathname.startsWith("/admin");
+
+  return (
+    <main className={`${isAdminRoute ? "bg-[#111] w-full h-full" : "flex bg-[#111] w-full h-full"}`}>
+      {!isAdminRoute && <Sidebar />}
+      {isAdminRoute && <AdminHeader />}
+      <div className={isAdminRoute ? "block w-full" : "flex-grow"}>
+        {children}
+      </div>
+    </main>
+  );
+}
