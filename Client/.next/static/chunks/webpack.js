4a065f058a0a8abcf477f9a7198f9d6c2a817538
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-10-05 16:30:03 -0400

    courses section of admin panel done

--- Client/.next/static/chunks/webpack.js
+++ Client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e598982c0e2f207"; }
+/******/ 		__webpack_require__.h = function() { return "d5bacba793749ffd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
