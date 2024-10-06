fc6f4d08b5ba2802403a47d79a0d9f37f4a03a4f
Michael Rosanelli <michaelrosanelli105@gmail.com>
2024-10-06 11:42:01 -0400

    more thingys :)

--- Server/models/module.js
+++ Server/models/module.js
@@ -1,4 +1,5 @@
 const Quiz = require('../models/quiz');
+const Course = require('../models/course');
 const mongoose = require('mongoose');
 const { Schema } = mongoose
 
@@ -15,6 +16,16 @@ let moduleSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: 'Course',
+        required: true
+    },
+    quiz: {
+        type: Schema.Types.ObjectId,
+        ref: 'Quiz',
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Module', moduleSchema);
