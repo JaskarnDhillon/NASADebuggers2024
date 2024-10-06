2d6e1bd692706e27c6e7c2716d8f604585728eb3
Beni Noje <beninoje@hotmail.com>
2024-10-06 12:43:48 -0400

    landing page done

--- Client/app/(root)/layout.tsx
+++ Client/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import StarsCanvas from "@/components/StarCanvas";
 
@@ -13,6 +14,7 @@ export default function RootLayout({
                 <Header />
                 
           {children}
+          <Footer/>
       </main>
     );
   }
