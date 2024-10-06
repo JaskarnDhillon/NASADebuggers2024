85420f3cce4ef2b26d36f1721c4724c518598cc2
Jack <jackdouma0@gmail.com>
2024-10-06 17:57:44 -0400

    module 1

---
+++ Client/app/(platform)/components/m1c4.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const m1c4 = () => {
+    return (
+        <div>
+            <h2>Importance of SWOT</h2>
+            <ul>
+                <li>Detecting New Ocean Features: By mapping the ocean's surface, SWOT helps scientists discover new oceanic phenomena and features.</li>
+                <li>Enabling Data Applications: The data collected is essential for a variety of applications, including flood forecasting, drought monitoring, and climate modeling.</li>
+                <li>Assessing Freshwater Resources: SWOT offers insights into freshwater availability, critical for water management and environmental conservation.</li>
+            </ul>
+        </div>
+    )
+  }
+  
+  export default m1c4
---
+++ Client/app/(platform)/components/m1c5.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const m1c5 = () => {
+    return (
+        <div>
+            <h2>Mission Goals</h2>
+            <p>The primary goal of SWOT is to provide detailed information about water resources, contributing to better management and understanding of Earth's water systems. The mission aims to gather precise measurements of water bodies globally, helping scientists predict changes in freshwater and ocean levels.</p>
+        </div>
+    )
+  }
+  
+  export default m1c5
