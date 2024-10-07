66fe064eacd2fd5267b14b4250cbe0292ee1ee5b
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-10-07 16:18:59 -0400

    fixed backend cors

--- Server/server.js
+++ Server/server.js
@@ -4,21 +4,23 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use(morgan('combined'));
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+// const clientURL = process.env.NODE_ENV === 'production' ? process.env.PROD_CLIENT_URL : process.env.DEV_CLIENT_URL;
 
-  const cors = require('cors');
-  app.use(cors({
-      origin: 'http://localhost:3000',
-      methods: 'GET,POST,PUT,DELETE,HEAD,OPTIONS',
-  }));
-}
+app.use(cors({
+  origin: ["http://localhost:3000", "https://nasa-debuggers2024.vercel.app"],
+  methods: 'GET,POST,PUT,DELETE,HEAD,OPTIONS',
+}));
 
 mongoose.connect(process.env.CONNECTION_STRING, {})
   .then((res) => { console.log('Connected to MongoDB') })
